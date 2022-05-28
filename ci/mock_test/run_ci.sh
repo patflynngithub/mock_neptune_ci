146289d4bfae3de33e6d51300b3e55bf5b91ed4c
Patrick Flynn <patrick.flynn.ctr@nrlmry.navy.mil>
2022-05-27 18:08:54 -0700

    add check timing capability

--- ci/mock_test/run_ci.sh
+++ ci/mock_test/run_ci.sh
@@ -30,7 +30,6 @@ set -x
 
 build_neptune_fcst ${NEPTUNE_TOP} ${CI_DIR}
 
-
 set +x
 echo
 echo "#########################################################"
@@ -67,6 +66,7 @@ ln -sf ${CI_RUN} ${CI_DIR}/${test_dir}
 
 setup_test ${CI_DIR} ${CI_RUN}
 
+# save CI_RUN is case there are ever multiple test runs
 EXPER01=${CI_RUN}
 
 run_test ${CI_DIR} ${CI_RUN} succeed output_matrix_file_accurate good_timing
@@ -96,7 +96,7 @@ echo "*                                                         *"
 echo "***********************************************************"
 set -x
 
-python ${BIN_DIR}/check_data_result.py ${CI_RUN}/matrix.txt
+python ${BIN_DIR}/check_data_result.py ${EXPER01}/matrix.txt
 
 set +x
 echo
@@ -109,4 +109,110 @@ echo "*                                                         *"
 echo "***********************************************************"
 set -x
 
+#------------------------------------------------------------------
+# Assess test execution timing(s)
+#------------------------------------------------------------------
+ 
+set +x
+echo
+echo "*********************************************************"
+echo "*                                                       *"
+echo "*           Assess test execution timings               *"
+echo "*                                                       *"
+echo "*                     STARTED                           *"
+echo "*                                                       *"
+echo "*********************************************************"
+set -x
+
+overall_timing_success=0
+
+set +e  # allows all timing assessments to be done despite failure
+        # in one of them
+
+# ------------
+
+# Mock Test Execution Timing Assessment
+
+set +x
+echo
+echo "*********************************************************"
+echo "*                                                       *"
+echo "*         Assess mock test execution timing             *"
+echo "*                                                       *"
+echo "*                     STARTED                           *"
+echo "*                                                       *"
+echo "*********************************************************"
+set -x
+
+TIMING_CEILING=100
+python ${BIN_DIR}/check_timing.py ${EXPER01}/nep.error.000000 ${TIMING_CEILING}
+timing_success=$?
+
+if [ $timing_success -ne 0 ]; then
+  overall_timing_success=$timing_success
+  echo "Timing assessment exit code: $timing_success"
+  echo "Timing ceiling exceeded in ${EXPER01}."
+  set +x
+  echo
+  echo "#########################################################"
+  echo "#                                                       #"
+  echo "#        Assess mock test execution timing              #"
+  echo "#                                                       #"
+  echo "#                     FAILURE                           #"
+  echo "#                                                       #"
+  echo "#########################################################"
+  set -x
+else
+  set +x
+  echo
+  echo "#########################################################"
+  echo "#                                                       #"
+  echo "#        Assess mock test execution timing          #"
+  echo "#                                                       #"
+  echo "#                     SUCCESS                           #"
+  echo "#                                                       #"
+  echo "#########################################################"
+  set -x
+fi
+
+# -----------
+
+if [ $overall_timing_success -eq 0 ]; then
+   set +x
+   echo
+   echo "#########################################################"
+   echo "#                                                       #"
+   echo "#           Assess test execution timings               #"
+   echo "#                                                       #"
+   echo "#                     SUCCESS                           #"
+   echo "#                                                       #"
+   echo "#########################################################"
+   set -x
+else
+   set +x
+   echo
+   echo "#########################################################"
+   echo "#                                                       #"
+   echo "#           Assess test execution timings               #"
+   echo "#                                                       #"
+   echo "#                     FAILURE                           #"
+   echo "#                                                       #"
+   echo "#########################################################"
+   set -x
+   exit 1 
+fi
+
+#------------------------------------------------------------------
+# Successfull Build and Testing!!!
+#------------------------------------------------------------------
+ 
+set +x
+echo
+echo "#########################################################"
+echo "#                                                       #"
+echo "#             Successful Build and Testing              #"
+echo "#                                                       #"
+echo "#########################################################"
+echo
+set -x
 
